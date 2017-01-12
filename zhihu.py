2792ee0abf2e075b18a52db188a803166efeafae
李亚松 <liyasong@rongcloud.cn>
2017-01-12 14:53:35 +0800

    修正目录 tobe_classfied 不存在问题
    当前逻辑是如果tobe_classfied存在，删除重建, 但是如果不存在则不会新建
    修正逻辑为，如果tobe_classfied存在则删除，新建tobe_classfied

--- zhihu.py
+++ zhihu.py
@@ -8,7 +8,7 @@ import requests
 import numpy as np
 import os
 from PIL import Image
-import shutil 
+import shutil
 import re
 
 # dimensions of our images.
@@ -45,13 +45,13 @@ mapArray = np.array(mapList)
 
 
 def login():
-    global s 
+    global s
     s = requests.session()
     homeReq = s.get(url, headers=headers)
     homeSoup = BeautifulSoup(homeReq.text, 'lxml')
     xsrf = homeSoup.find('input',{'name':'_xsrf','type':'hidden'})['value']
     data['_xsrf'] = xsrf
-    timeStamp = int(time.time()*1000)    
+    timeStamp = int(time.time()*1000)
     captchaUrl = url + '/captcha.gif?=' + str(timeStamp)
     f = open('captcha.gif','wb')
     f.write(s.get(captchaUrl, headers=headers).content)
@@ -110,8 +110,8 @@ def predict(cropModel,classModel):
     target.sort()
     if os.path.exists('./tobe_classfied'):
         shutil.rmtree('./tobe_classfied')
-        os.mkdir('tobe_classfied')
-        os.mkdir('./tobe_classfied/data')
+    os.mkdir('tobe_classfied')
+    os.mkdir('./tobe_classfied/data')
     imList = []
     for each in target:
         imList.append(str(each) + '.png')
